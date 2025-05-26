fdadf3b1c9f33ff58d0db087dd2e8f01b062fd3b
imelda novianty <noviantyimelda32@gmail.com>
2025-05-26 11:29:17 +0700

    first commit

--- kuis/KuisMahasiswa.java
+++ kuis/KuisMahasiswa.java
@@ -1,61 +1,63 @@
 
 
-// package kuis;
-// import java.util.Scanner;
+package kuis;
+import java.util.Scanner;
 
-// public class KuisMahasiswa {
+public class KuisMahasiswa {
 
-//     static class Mahasiswa {
-//         private String nama;
-//         private String nim;
-//         private double ipk;
+    static class Mahasiswa {
+        private String nama;
+        private String nim;
+        private double ipk;
 
-//         public void setNama(String nama) {
-//             this.nama = nama;
-//         }
+        public void setNama(String nama) {
+            this.nama = nama;
+        }
 
-//         public void setNim(String nim) {
-//             this.nim = nim;
-//         }
+        public void setNim(String nim) {
+            this.nim = nim;
+        }
 
-//         public void setIpk(double ipk) {
-//             this.ipk = ipk;
-//         }
+        public void setIpk(double ipk) {
+            this.ipk = ipk;
+        }
 
-//         public String getNama() {
-//             return nama;
-//         }
+        public String getNama() {
+            return nama;
+        }
 
-//         public String getNim() {
-//             return nim;
-//         }
+        public String getNim() {
+            return nim;
+        }
 
-//         public double getIpk() {
-//             return ipk;
-//         }
-//     }
+        public double getIpk() {
+            return ipk;
+        }
+    }
 
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         Mahasiswa mhs = new Mahasiswa();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Mahasiswa mhs = new Mahasiswa();
 
-//         System.out.println("=== Input Data Mahasiswa ===");
-//         System.out.print("Masukkan Nama: ");
-//         String nama = scanner.nextLine();
+        System.out.println("=== Input Data Mahasiswa ===");
+        System.out.print("Masukkan Nama: ");
+        String nama = scanner.nextLine();
 
-//         System.out.print("Masukkan NIM: ");
-//         String nim = scanner.nextLine();
+        System.out.print("Masukkan NIM: ");
+        String nim = scanner.nextLine();
 
-//         System.out.print("Masukkan IPK: ");
-//         double ipk = scanner.nextDouble();
+        System.out.print("Masukkan IPK: ");
+        double ipk = scanner.nextDouble();
 
-//         mhs.setNama(nama);
-//         mhs.setNim(nim);
-//         mhs.setIpk(ipk);
+        mhs.setNama(nama);
+        mhs.setNim(nim);
+        mhs.setIpk(ipk);
 
-//         System.out.println("\n=== Data Mahasiswa ===");
-//         System.out.println("Nama: " + mhs.getNama());
-//         System.out.println("NIM: " + mhs.getNim());
-//         System.out.println("IPK: " + mhs.getIpk());
-//     }
-// }
+        System.out.println("\n=== Data Mahasiswa ===");
+        System.out.println("Nama: " + mhs.getNama());
+        System.out.println("NIM: " + mhs.getNim());
+        System.out.println("IPK: " + mhs.getIpk());
+    }
+}
+
+
